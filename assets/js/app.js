e17b4c6da78ed42d260df2f2ac256d54440b6441
bywillsilva <raniwillere@gmail.com>
2023-02-25 19:10:20 -0300

    Atualizando css

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,74 +5,85 @@ const body = document.querySelector("body");
 body.addEventListener("keydown", (e) => {
     for (let index = 0; index <= 10; index++) {
         if (e.code == "Numpad" + index || e.code == "Digit" + index) {
-            typer.innerText += index
+            setInto(index);
         }
     }
 
     if (e.code == "NumpadAdd") {
-        setInto("+")
+        setInto("+");
     }
 
     if (e.code == "NumpadSubtract") {
-        setInto("-")
+        setInto("-");
     }
     if (e.code == "NumpadMultiply") {
-        setInto("*")
+        setInto("*");
     }
     if (e.code == "NumpadDivide") {
-        setInto("/")
+        setInto("/");
     }
     if (e.code == "NumpadEnter") {
-        getEqual()
+        getEqual();
     }
     if (e.code == "NumpadDecimal") {
-        setInto('.')
+        setInto(".");
     }
     if (e.code == "Backspace") {
-        eraseBack()
+        eraseBack();
     }
-})
+});
 
 function setInto(item) {
-    if (item == "+" || item == "-" || item == "*" || item == "/") {
-        if (hist.textContent.includes(item)) {
-            return getEqual(item)
+    if (typer.textContent.length < 8) {
+        if (item == "+" || item == "-" || item == "*" || item == "/" || item == "%") {
+            if (hist.textContent.includes(item)) {
+                return getEqual(item);
+            }
+            if (hist.textContent.substring(0) != item) {
+                hist.innerText = typer.textContent + item;
+                return setEmpty(0);
+            }
         }
-        if (hist.textContent.substring(0) != item) {   
-            hist.innerText = typer.textContent + item
-            return setEmpty(0)
-        }
+        typer.innerText += item;
     }
-    typer.innerText += item
 }
 
 function getEqual(item) {
     if (typer.textContent != "") {
-        if (((hist.textContent + typer.textContent).includes('/0') == false)) {
+        if ((hist.textContent + typer.textContent).includes("/0") == false) {
             if (typer.textContent.includes(item)) {
-                typer.innerText = eval(hist.textContent + typer.textContent.substring(0, (typer.textContent.length) - 1))
+                typer.innerText = eval(
+                    hist.textContent +
+                        typer.textContent.substring(
+                            0,
+                            typer.textContent.length - 1
+                        )
+                );
             } else {
-                typer.innerText = eval(hist.textContent + typer.textContent)
+                typer.innerText = eval(hist.textContent + typer.textContent);
             }
-        
-            setEmpty(1)
+
+            setEmpty(1);
         } else {
-            typer.innerText = "E!"
+            typer.innerText = "E!";
         }
     }
 }
 
 function setEmpty(deletHist) {
     if (deletHist == 0) {
-        return typer.innerText = ""
+        return (typer.innerText = "");
     } else if (deletHist == 1) {
-        return hist.innerText = ""
+        return (hist.innerText = "");
     }
 
-    typer.innerText = ""
-    hist.innerText = ""
+    typer.innerText = "";
+    hist.innerText = "";
 }
 
 function eraseBack() {
-    return typer.innerText = typer.textContent.substring(0, (typer.textContent.length)-1)
+    return (typer.innerText = typer.textContent.substring(
+        0,
+        typer.textContent.length - 1
+    ));
 }
